f9eff433e63a962057744f734936bc373a6b0271
Arkadiusz Bokowy <arkadiusz.bokowy@gmail.com>
2017-10-19 09:30:16 +0200

    Declarations for library internal function

--- include/aptx422.h
+++ include/aptx422.h
@@ -110,6 +110,34 @@ typedef struct aptX_encoder_422_t {
 	aptX_QMF_analyzer_422 analyzer[APTX_CHANNELS];
 } __attribute__ ((packed)) aptX_encoder_422;
 
+/* Functions (theoretically) available in the apt-X library. Some of them
+ * might not be present in the particular version of the apt-X library due
+ * to compiler optimizations. Also note, that not all of them are public. */
+int32_t updateCodewordHistory(const int32_t a[4], int32_t codeword);
+int32_t generateDither(int32_t codeword, int32_t dither[4]);
+uint16_t packCodeword(const aptX_subband_encoder_422 *e);
+void AsmQmfConvO(const int16_t a1[16], const int16_t a2[16], const int32_t coeffs[16], int32_t out[3]);
+void AsmQmfConvI(const int32_t a1[16], const int32_t a2[16], const int32_t coeffs[16], int32_t out[2]);
+void QmfAnalysisFilter(const int32_t pcm[4], aptX_QMF_analyzer_422 *a, const int32_t refs[4], int32_t diff[4]);
+int32_t BsearchLL(uint32_t a, int32_t b, const int32_t data[65]);
+int32_t BsearchLH(uint32_t a, int32_t b, const int32_t data[9]);
+int32_t BsearchHL(uint32_t a, int32_t b, const int32_t data[3]);
+int32_t BsearchHH(uint32_t a, int32_t b, const int32_t data[5]);
+void quantiseDifferenceLL(int32_t diff, int32_t dither, int32_t c, aptX_quantizer_422 *q);
+void quantiseDifferenceLH(int32_t diff, int32_t dither, int32_t c, aptX_quantizer_422 *q);
+void quantiseDifferenceHL(int32_t diff, int32_t dither, int32_t c, aptX_quantizer_422 *q);
+void quantiseDifferenceHH(int32_t diff, int32_t dither, int32_t c, aptX_quantizer_422 *q);
+void aptxEncode(int32_t pcm[4], aptX_QMF_analyzer_422 *a, aptX_subband_encoder_422 *e);
+void insertSync(aptX_subband_encoder_422 *e1, aptX_subband_encoder_422 *e2, int32_t *sync);
+void invertQuantisation(int32_t a, int32_t dither, aptX_inverter_422 *i);
+void performPredictionFiltering(int32_t a, aptX_prediction_filter_422 *f);
+void performPredictionFilteringLL(int32_t a, aptX_prediction_filter_422 *f);
+void performPredictionFilteringHL(int32_t a, aptX_prediction_filter_422 *f);
+void processSubband(int32_t a, int32_t dither, aptX_prediction_filter_422 *f, aptX_inverter_422 *i);
+void processSubbandLL(int32_t a, int32_t dither, aptX_prediction_filter_422 *f, aptX_inverter_422 *i);
+void processSubbandHL(int32_t a, int32_t dither, aptX_prediction_filter_422 *f, aptX_inverter_422 *i);
+void aptxPostEncode(aptX_subband_encoder_422 *e);
+
 #ifdef __cplusplus
 }
 #endif
